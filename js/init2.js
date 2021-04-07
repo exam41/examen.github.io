e3389df4951f1948ce59405d0a3d494fd1f0d86b
exam41 <82076313+exam41@users.noreply.github.com>
2021-04-07 00:06:42 -0500

    Create init2.js

---
+++ js/init2.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDNiabvL2D0O-d7KTHqKHfuvFz0fxn9Uyo",
+    authDomain: "autentifica41-56fd4.firebaseapp.com",
+    projectId: "autentifica41-56fd4",
+    storageBucket: "autentifica41-56fd4.appspot.com",
+    messagingSenderId: "933055637171",
+    appId: "1:933055637171:web:f089ba90705d6b9a5b32c6",
+    measurementId: "G-5CC6S1RE2Z"
+    });
